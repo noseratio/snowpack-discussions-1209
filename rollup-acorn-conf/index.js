03cbc0d8326271e54b664ac600baf0f3df0e350d
Andrew Nosenko <noseratio@gmail.com>
2020-10-24 00:37:29 +1100

    added rollup-acorn-conf

---
+++ rollup-acorn-conf/index.js
@@ -0,0 +1,15 @@
+﻿module.exports = function plugin (options = {}) {
+  return { 
+    name: 'rollup-acorn-conf',
+
+    options: (opts) => { 
+      console.log("Enabling 'acorn-stage3'...");
+      opts.acorn = opts.acorn ?? {};
+      opts.acorn.ecmaVersion = 2020;
+      opts.acornInjectPlugins = opts.acornInjectPlugins ?? [];
+      opts.acornInjectPlugins.push(require('acorn-stage3'));
+      return opts;
+    }
+  };
+};
+
---
+++ snowpack.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_"
+  },
+
+  installOptions: {
+    rollup: { 
+      plugins: [require('rollup-acorn-conf')()]
+    }
+  }
+}
